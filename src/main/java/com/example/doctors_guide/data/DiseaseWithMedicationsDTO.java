80ee0c550d14ae9b007431c0a842161100d5a6e5
victoriadobryden <vikidobryden@gmail.com>
2024-04-01 23:58:29 +0300

    added methods and WebController mappings

---
+++ src/main/java/com/example/doctors_guide/data/DiseaseWithMedicationsDTO.java
@@ -0,0 +1,2 @@
+package com.example.doctors_guide.data;public class DiseaseWithMedicationsDTO {
+}
---
+++ src/main/java/com/example/doctors_guide/data/InterchangeableMedicationId.java
@@ -0,0 +1,4 @@
+package com.example.doctors_guide.data;
+
+public class InterchangebleMedicationId {
+}
